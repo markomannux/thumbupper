4cd09a17e6424f16f8227c5579028bd6ed8898bb
markomannux <markomannux@gmail.com>
2011-11-03 18:37:24 +0100

    trying spock tests

---
+++ src/test/groovy/goal/GoalTests.groovy
@@ -0,0 +1,23 @@
+package goal
+
+import groovyx.gaelyk.spock.*
+import com.google.appengine.api.datastore.*
+import static com.google.appengine.api.datastore.FetchOptions.Builder.*
+ 
+class GoalTests extends GaelykUnitSpec {
+ 
+    def setup() {
+        groovlet 'goal/list.groovy'
+    }
+ 
+    def "the groovlet is used to obtain a list of goals"() {
+        given: "some goals exists in the datastore"
+        
+        when: "the initialised groovlet is invoked"
+        groovletInstance.get()
+         
+        then: "the persisted data is found in the response"
+	println groovletInstance.response
+
+    }
+}
